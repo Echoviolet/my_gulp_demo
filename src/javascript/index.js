ae9f8534353f0e6486ff5d253ac873386558fb98
Echo <echo@server.melody.private>
2016-11-30 16:18:49 +0800

    finish index

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,90 +1,40 @@
 /**
  * Created by echo on 16/11/21.
  */
-//jquery.digital
-(function($) {
-    $.fn.extend({
-        digital: function(text) {
-            var self = $(this);
-            var chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-            var original = text;
 
-            for (var i = 0; i < text.length; i++) {
-                var rChar = chars[Math.floor(Math.random() * chars.length)];
-                if (text[i] !== ' ') text = setCharAt(text, i, rChar);
-            }
+$(document).ready(function(){
+   'use strict';
 
-            function writeText() {
-                self.empty();
+   var nIntervId = setInterval(randomNumber,1000/16);
+   function randomNumber(){
+       var k = $('.c0').text(Math.floor(Math.random()*10));
+       var n = $('.c1').text(Math.floor(Math.random()*10));
+       var o= $('.c2').text(Math.floor(Math.random()*10));
+       var w =$('.c3').text(Math.floor(Math.random()*10));
+       var p = $('.c5').text(Math.floor(Math.random()*10));
+       var e = $('.c6').text(Math.floor(Math.random()*10));
+       var o1 = $('.c7').text(Math.floor(Math.random()*10));
+       var p1 = $('.c8').text(Math.floor(Math.random()*10));
+       var l = $('.c9').text(Math.floor(Math.random()*10));
+       var e1 = $('.c10').text(Math.floor(Math.random()*10));
+       // total();
+       // function total(){
+       //     if(k == 0 || n == 1 || o == 2){
+       //         clearInterval(nIntervId);
+       //     }
+       // }
 
-                var space = false;
-                var postSpace = false;
-                var classes = 'c';
-                for (var i = 0; i < text.length; i++) {
-                    if (text[i] === ' ') {
-                        postSpace = true;
-                        space = true;
-                    }
+   }
 
-                    if (postSpace) {
-                        classes = 'c white';
-                    }
-
-                    if (space) {
-                        classes = 'c s';
-                        space = false;
-                    }
-
-                    self.append('<span class="' + classes + ' c' + i + '">' + text[i] + '</span>');
-                }
-            }
-
-            var changeIteration = 0;
-
-            function changeChar() {
-                if (changeIteration % 4 === 0 && changeIteration > 20) {
-                    var same = true;
-
-                    while (same) {
-                        var index = Math.floor(Math.random() * text.length);
-                        if (text[index] !== original[index]) same = false;
-                    }
-
-                    text = setCharAt(text, index, original[index]);
-                } else {
-                    for (var i = 0; i < text.length; i++) {
-                        if (text[i] !== original[i]) {
-                            var same = true;
-
-                            while (same) {
-                                var rChar = chars[Math.floor(Math.random() * chars.length)];
-                                var index = Math.floor(Math.random() * text.length);
-                                if (text[index] !== chars[index]) same = false;
-                            }
-
-                            text = setCharAt(text, i, rChar);
-                        }
-                    }
-                }
-
-                writeText();
-
-                if (text === original) {
-                    return false;
-                } else {
-                    setTimeout(function() {
-                        changeChar();
-                    }, 1000 / 16);
-                    changeIteration++;
-                }
-            }
-
-            function setCharAt(str, index, chr) {
-                if (index > str.length - 1) return str;
-                return str.substr(0, index) + chr + str.substr(index + 1);
-            }
-
-            changeChar();
-        }
+    $(window).resize(function() {
+        focusBackgroundReposition();
     });
-})(jQuery);
+
+    focusBackgroundReposition();
+    function focusBackgroundReposition() {
+        var offset = -295;
+        var newPosition = ($(window).width() / 2) + offset;
+        $('div#focus-container').css({'background-position': newPosition + 'px top'});
+    }
+
+});
